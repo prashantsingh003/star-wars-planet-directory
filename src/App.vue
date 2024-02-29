c6279d2d7cc336dee401f022435c1bc43cafe48f
Prashant Singh <53073860+prashantsingh003@users.noreply.github.com>
2024-02-29 21:03:43 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script>
+import HelloWorld from './components';
+export default{}
+</script>
+
+<template>
+</template>
+
+<style>
+@import './main.scss'
+</style>
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+    <div class="scroll-text">
+      <h1>STAR WARS</h1>
+      <h2>{{content.title}}</h2>
+      <p v-for="(para,index) in content.paragraphs" :key="index+'para'">
+				{{ para }}
+			</p>
+		</div>
+  </div>
+</template>
+
+<script>
+export default{
+	name:'LandingPage',
+	data(){
+		return{
+			content:{
+				title:'Scrolling Text Effect',
+				paragraphs:[
+					'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!',
+					'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!',
+					'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!',
+					'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!',
+					'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!',
+					'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad maiores aut cupiditate unde ipsam ut fugit deleniti non autem, ab, tempore ratione quo, nihil delectus sequi incidunt voluptates quis. Expedita!'
+				]
+			}
+		}
+	},
+}
+</script>
